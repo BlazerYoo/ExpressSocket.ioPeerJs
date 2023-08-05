fb534834e56a41dead0d1be343df28a148ef13da
Boaz Yoo <blazeryoo1921@gmail.com>
2023-08-04 21:17:57 -0400

    add connection error handling

--- server.js
+++ server.js
@@ -93,6 +93,16 @@ io.on('connection', (socket) => {
 
 
 
+// Connection is abnormally closed
+io.engine.on("connection_error", (err) => {
+    console.log(err.req);      // the request object
+    console.log(err.code);     // the error code, for example 1
+    console.log(err.message);  // the error message, for example "Session ID unknown"
+    console.log(err.context);  // some additional error context
+});
+
+
+
 // Start server
 server.listen(process.env.PORT || 3000, () => {
     console.log(`App started listening on port ${server.address().port}`);
